a9869e4924ca2c953c907d6a1c805e45423224dd
等 <2900351160@qq.com>
2020-01-16 16:25:00 +0800

    add

---
+++ onJava/src/collections/ArrayIsNotIterable.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.Arrays;
+
+public class ArrayIsNotIterable {
+    static<T> void test(Iterable<T> ib){
+        for (T t:ib){
+            System.out.print(t+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        test(Arrays.asList(1,2,3));
+        String[] strings = {"a","B","C"};
+        test(Arrays.asList(strings));
+    }
+}
---
+++ onJava/src/files/PartsOfPaths.java
@@ -0,0 +1,18 @@
+package files;
+import java.nio.file.*;
+public class PartsOfPaths {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("os.name"));
+        Path p = Paths.get("PartsOfPaths.java").toAbsolutePath();
+        System.out.println("p=="+p);
+        for(int i = 0; i < p.getNameCount(); i++)
+            System.out.println(p.getName(i));
+        System.out.println("ends with '.java': " + p.endsWith(".java"));
+        for(Path pp : p) {
+            System.out.print(pp + ": ");
+            System.out.print(p.startsWith(pp) + " : ");
+            System.out.println(p.endsWith(pp));
+        }
+        System.out.println("Starts with " + p.getRoot() + " " + p.startsWith(p.getRoot()));
+    }
+}
